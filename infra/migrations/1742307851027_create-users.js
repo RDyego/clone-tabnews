2e5c5bcf0f043072b62d2854b4370687f186cdd9
RDyego <dyego@alu.ufc.br>
2025-04-19 11:21:42 -0300

    fix(migration): adjust users table schema
    - Change password field length from 72 to 60 characters
    - Set created_at and updated_at fields as not null
    - Use UTC timezone for created_at and updated_at default values

--- infra/migrations/1742307851027_create-users.js
+++ infra/migrations/1742307851027_create-users.js
@@ -16,16 +16,18 @@ exports.up = (pgm) => {
       unique: true,
     },
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
